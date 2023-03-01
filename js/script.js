5111f5f78c850de8517b73f8c4a034a25176c212
Paweł Dziembek <paweldziembek@gmail.com>
2023-03-01 20:57:43 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log("Cześć");
+
+let przycisk = document.querySelector(".przycisk");
+let naglowek = document.querySelector(".naglowek");
+
+przycisk.addEventListener("click", () => {
+    naglowek.remove();
+})
+
+
+console.log(przycisk); 
